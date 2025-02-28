64f3eaa31431153a02480649cb89295c1cd71887
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-02-28 13:58:36 +0100

    Initial commit

---
+++ js/download.js
@@ -0,0 +1,10 @@
+const btnDonwload = document.querySelector(".btn-download");
+
+btnDonwload.addEventListener("click", function(){
+    const link = document.createElement("a");
+    link.href = "assets/CV_Salah-Wassim.pdf"; // URL du fichier à télécharger
+    link.download = "CV_Salah-Wassim ARFA.pdf"; // Nom du fichier téléchargé
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+})
