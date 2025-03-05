e2a1c2630298018e30d29d5f4a3c1e7baca005f7
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-03-05 08:12:59 +0100

    Creating clickable project titles

--- js/works.js
+++ js/works.js
@@ -3,6 +3,7 @@ const worksData = [
         scr: "assets/article.PNG",
         alt:"Projet Create Your Team",
         title: "Create Your Team",
+        href:"",
         tagYear: "2024",
         tagTechnos:["HTML", "CSS", "JS"],
         description:"Create Your Team permet de créer des équipes. Projet qui explore le concept de drag and drop"
@@ -11,6 +12,7 @@ const worksData = [
         scr: "assets/article.PNG",
         alt:"Projet PGCD",
         title: "PGCD (Plus Grand Commun Diviseur)",
+        href:"",
         tagYear: "2023",
         tagTechnos:["HTML", "CSS", "JS", "Jenkins", "SEO", "Vercel"],
         description:"PGCD est une application web qui permet de calculer rapidement et gratuitement le PGCD et le PPCM de deux ou trois entiers"
@@ -19,17 +21,38 @@ const worksData = [
         scr: "assets/article.PNG",
         alt:"Projet SalahBook",
         title: "SalahBook - Blog",
+        href:"",
         tagYear: "2021",
         tagTechnos:["PHP", "Composer", "HTML5", "CSS3", "Javascript", "MySQL"],
         description:"Blog en cours de création. Projet réalisé sans framework afin d’intégrer manuellement des mécanismes fonctionnels et de sécurité, tout en approfondissant la compréhension des concepts fondamentaux."
     },
+    {
+        scr: "assets/article.PNG",
+        alt:"Projet ParkLib",
+        title: "Park'Lib",
+        href:"",
+        tagYear: "2021",
+        tagTechnos:["React Native", "Node.js", "Angular", "Redis", "Docker", "MySQL"],
+        description:"Park'Lib est une application qui permet de s'informer sur les parkings public d'une ville et de faire de la location de places de parkings entre particulier"
+    },
+    {
+        scr: "assets/article.PNG",
+        alt:"Projet Bot Discord",
+        title: "Bot Discord",
+        href:"",
+        tagYear: "2022",
+        tagTechnos:["Javascript"],
+        description:"Description à venir"
+    },
 ];
 
-function generateWorks() {
-    const container = document.getElementById("worksContainer");
+const recentWorksData = worksData.slice(0, 3);
+
+function generateWorks(idElement, array) {
+    const container = document.getElementById(idElement);
     container.innerHTML = "";
 
-    worksData.forEach(work => {
+    array.forEach(work => {
         const workCard = document.createElement("div");
         workCard.classList.add("work-card");
 
@@ -39,7 +62,7 @@ function generateWorks() {
             </div>
             <div class="work-card-text">
                 <div class="work-space work-card-header">
-                    <p>${work.title}</p>
+                    <a href="${work.href}"><p>${work.title}</p></a>
                 </div>
                 <div class="work-space work-card-tag">
                     <span class="tag-year">${work.tagYear}</span>
@@ -57,4 +80,4 @@ function generateWorks() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", generateWorks);
+document.addEventListener("DOMContentLoaded", generateWorks("worksContainer", recentWorksData));
