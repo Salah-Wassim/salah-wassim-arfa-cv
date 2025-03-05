a4f7dcbc418fdce508bb25f04e66062507e208a5
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-03-05 15:51:49 +0100

    Rename voir-plus.html to portfolio.html. Rename voir-plus folder to portfolio. Redirect paths updated. Portfolio link added to header.

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@ const btnContact = document.querySelector(".btn-contact");
 
 btnContact.addEventListener("click", function(){
     const aElement = document.createElement("a");
-    aElement.href = "voir-plus/contact.html";
+    aElement.href = "portfolio/contact.html";
     aElement.rel = "noopener";
     document.body.appendChild(aElement)
     aElement.click();
--- js/learn-more.js
+++ js/learn-more.js
@@ -2,7 +2,7 @@ const btnLearnMore = document.querySelector(".learn-more");
 
 btnLearnMore.addEventListener("click", function(){
     const link = document.createElement("a");
-    link.href = "voir-plus.html";
+    link.href = "portfolio.html";
     link.rel = "noopener"
     document.body.appendChild(link);
     link.click();
