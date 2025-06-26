b0744b623c77940029540305be7bfe64084bb779
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-06-26 10:05:39 +0200

    CV file update. Modification of the download link

--- js/download.js
+++ js/download.js
@@ -2,7 +2,7 @@ const btnDonwload = document.querySelector(".btn-download");
 
 btnDonwload.addEventListener("click", function(){
     const link = document.createElement("a");
-    link.href = "assets/CV_Salah-Wassim.pdf"; // URL du fichier à télécharger
+    link.href = "assets/CV_Salah-Wassim ARFA.pdf"; // URL du fichier à télécharger
     link.download = "CV_Salah-Wassim ARFA.pdf"; // Nom du fichier téléchargé
     document.body.appendChild(link);
     link.click();
