2ae7e3aaa04bf7b24bfa239a154f9d42341f695e
Salah-Wassim <salahwassim.arfae1@gmail.com>
2026-01-17 08:03:41 +0100

    Fix applied to the display of the CV iframe. Affichage l'iframe dans le dom de manière dynamique

--- js/manage-cookies-consent.js
+++ js/manage-cookies-consent.js
@@ -2,16 +2,50 @@ const iframeContainer = document.querySelector('.iframe-cv-container')
 const cookieCard = document.querySelector(".cookie-card-container");
 const btnAcceptCookies = document.querySelector(".accept")
 
+const linkCvIframe = "https://www.canva.com/design/DAGq53ZgUrk/8lRVVfkd-R4BLCxKs4hqvA/view?embed"
+
 window.addEventListener("load", function(){
   if(this.localStorage.getItem('usr-accept-cookies', 'true')){
     iframeContainer.style.display = "block"
     cookieCard.style.display = "none"
+    
+    const iframe = document.createElement("iframe")
+    iframe.src = linkCvIframe
+    iframe.loading = "lazy"
+    iframe.allowfullscreen = "allowfullscreen"
+    iframe.allow="fullscreen"
+    iframe.style.position = "absolute"
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.top = "0"
+    iframe.style.left = "0"
+    iframe.style.border = "none"
+    iframe.style.padding = "0"
+    iframe.style.margin = "0"
+
+    iframeContainer.appendChild(iframe)
   }
 })
 
 btnAcceptCookies.addEventListener('click', function () {
     iframeContainer.style.display = "block"
     cookieCard.style.display = "none"
+
+    const iframe = document.createElement("iframe")
+    iframe.src = linkCvIframe
+    iframe.loading = "lazy"
+    iframe.allowfullscreen = "allowfullscreen"
+    iframe.allow="fullscreen"
+    iframe.style.position = "absolute"
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.top = "0"
+    iframe.style.left = "0"
+    iframe.style.border = "none"
+    iframe.style.padding = "0"
+    iframe.style.margin = "0"
+
+    iframeContainer.appendChild(iframe)
 
     if(iframeContainer.style.display === "block" && cookieCard.style.display === "none"){
         localStorage.setItem('usr-accept-cookies', 'true')
